65d8f34198e58f8dc282dfe40d222da43aa717f7
tonydefaria <me@tonydefaria.com>
2023-09-27 09:40:38 +0200

    add manifest file

--- components/favicon_component.js
+++ components/favicon_component.js
@@ -23,6 +23,7 @@ export default function FaviconComponent() {
       <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32" />
       <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16" />
       <link rel="icon" type="image/png" href="/favicons/favicon-128.png" sizes="128x128" />
+      <link rel="manifest" href="/favicons/manifest.json" />
     </Head>
   )
 }
